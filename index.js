a4a1b85416f8e0ea34090e7d08a24544304eae0b
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-13 10:32:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const app = express();
 const port = 3000;
 
 app.get('/api/visioncine', async (req, res) => {
-  const url = "https://www.visioncine-2.com/watch/make-heroine-ga-oosugiru"; // URL fixa
+  const url = "https://www.visioncine-2.com/watch/make-heroine-ga-oosugiru";
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, { timeout: 10000 }); 
     const html = response.data;
     const $ = cheerio.load(html);
 
-    // Extrair o título do anime
-    const animeTitle = $('h1.text-light').text().trim(); 
+    const animeTitle = $('h1.text-light').text().trim();
 
-    // Extrair lista de episódios
     const episodes = [];
     $('.ep').each((index, element) => {
       const episodeNumber = $(element).find('p[number]').text();
@@ -41,11 +39,12 @@ app.get('/api/visioncine', async (req, res) => {
 
     res.json(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching data:', error); 
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 });
 
+// Inicie o servidor apenas uma vez
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
