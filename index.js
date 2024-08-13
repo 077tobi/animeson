4fa7bfa546af00766ca9e1b186e8f889e65735b0
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-13 10:19:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-const app = express();
-const port = 3000;
-
-app.get('/api/anroll/latest', async (req, res) => {
-    const url = 'https://anroll.net/'; // URL da página inicial
+app.get('/api/visioncine/:animeId', async (req, res) => {
+    const animeId = req.params.animeId;
+    const url = `https://www.visioncine-2.com/watch/make-heroine-ga-oosugiru${animeId}`; // Substitua 'make-heroine-ga-oosugiru' pelo nome do anime
 
     try {
         const response = await axios.get(url);
         const html = response.data;
         const $ = cheerio.load(html);
 
+        // Extrair os dados desejados:
+        const animeTitle = $('h1.text-light').text().trim(); 
+
         // Extrair lista de episódios
         const episodes = [];
-        $('li.release-item').each((index, element) => {
-            const episodeLink = $(element).find('a').attr('href');
-            const animeTitle = $(element).find('h1').text().trim();
-            const episodeNumber = $(element).find('span.episode-badge b').text();
-            const imageUrl = $(element).find('img').attr('src');
+        $('.ep').each((index, element) => {
+            const episodeNumber = $(element).find('p[number]').text();
+            const episodeLink = $(element).find('a.btn.free').attr('href');
+            const episodeDuration = $(element).find('.info p:nth-child(1)').text().trim(); 
+            const episodePublished = $(element).find('.info p:nth-child(2)').text().trim();
+            const episodeImageUrl = $(element).find('.image').attr('style').split('url(\'')[1].split('\')')[0]; 
 
             episodes.push({
-                episodeLink,
-                animeTitle,
                 episodeNumber,
-                imageUrl, 
+                episodeLink,
+                episodeDuration,
+                episodePublished,
+                episodeImageUrl
             });
         });
 
-        res.json(episodes);
+        const data = {
+            animeTitle,
+            episodes
+        };
+
+        res.json(data);
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
 });
