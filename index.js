800ca491fc5e3c67f20c775436c0e35cf4f16f93
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-13 10:49:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,27 @@ const cheerio = require('cheerio');
 const app = express();
 const port = 3000;
 
-app.get('/api/visioncine', async (req, res) => {
-  const url = "https://www.visioncine-2.com/watch/make-heroine-ga-oosugiru";
+app.get('/api/animefire/:animeId', async (req, res) => {
+  const animeId = req.params.animeId;
+  const url = `https://animefire.plus/animes/${animeId}-todos-os-episodios`;
 
   try {
-    const response = await axios.get(url, { timeout: 10000 }); 
+    const response = await axios.get(url, { timeout: 10000 });
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const animeTitle = $('h1.text-light').text().trim();
+    // Extrair o título do anime
+    const animeTitle = $('h1.page-title').text().trim();
 
+    // Extrair a lista de episódios
     const episodes = [];
-    $('.ep').each((index, element) => {
-      const episodeNumber = $(element).find('p[number]').text();
-      const episodeLink = $(element).find('a.btn.free').attr('href'); 
-      const episodeDuration = $(element).find('.info p:nth-child(1)').text().trim();
-      const episodePublished = $(element).find('.info p:nth-child(2)').text().trim();
-      const episodeImageUrl = $(element).find('.image').attr('style').split('url(\'')[1].split('\')')[0];
+    $('.div_video_list a.lEp').each((index, element) => {
+      const episodeLink = $(element).attr('href');
+      const episodeTitle = $(element).text().trim();
 
       episodes.push({
-        episodeNumber,
         episodeLink,
-        episodeDuration,
-        episodePublished,
-        episodeImageUrl
+        episodeTitle
       });
     });
 
@@ -39,12 +36,11 @@ app.get('/api/visioncine', async (req, res) => {
 
     res.json(data);
   } catch (error) {
-    console.error('Error fetching data:', error); 
+    console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 });
 
-// Inicie o servidor apenas uma vez
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
