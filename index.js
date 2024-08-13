9df8a18fabaa0081f19db3cea8adf4ba3e59a7c0
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-13 09:31:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-   const express = require('express');
-   const axios = require('axios');
-   const cheerio = require('cheerio'); // Importar o Cheerio
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-   const app = express();
-   const port = 3000;
+const app = express();
+const port = 3000;
 
-   app.get('/api/anroll/:animeId', async (req, res) => {
-       const animeId = req.params.animeId; // Obter ID do anime da URL
-       const url = `https://anroll.net/e/${animeId}`;
+app.get('/api/anroll/latest', async (req, res) => {
+    const url = 'https://anroll.net/'; // URL da página inicial
 
-       try {
-           const response = await axios.get(url); 
-           const html = response.data;
-           const $ = cheerio.load(html); // Carregar o HTML no Cheerio
+    try {
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
 
-           // Extrair os dados desejados:
-           const imageUrl = $('img').attr('src'); // URL da imagem
-           const animeTitle = $('h1').text().trim(); // Título do anime
-           const episodeNumber = $('span.episode-badge b').text(); // Número do episódio
+        // Extrair lista de episódios
+        const episodes = [];
+        $('li.release-item').each((index, element) => {
+            const episodeLink = $(element).find('a').attr('href');
+            const animeTitle = $(element).find('h1').text().trim();
+            const episodeNumber = $(element).find('span.episode-badge b').text();
+            const imageUrl = $(element).find('img').attr('src');
 
-           const data = {
-               imageUrl,
-               animeTitle,
-               episodeNumber,
-           };
+            episodes.push({
+                episodeLink,
+                animeTitle,
+                episodeNumber,
+                imageUrl, 
+            });
+        });
 
-           res.json(data); // Retornar os dados como JSON
-       } catch (error) {
-           console.error('Error fetching data:', error);
-           res.status(500).json({ error: 'Failed to fetch data' });
-       }
-   });
+        res.json(episodes);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
 
-   app.listen(port, () => {
-       console.log(`Server listening at http://localhost:${port}`);
-   });
-   
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
