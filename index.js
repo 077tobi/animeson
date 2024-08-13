2933f048730168f77319b2c77359c2236307a313
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-13 11:18:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,36 +6,42 @@ const app = express();
 const port = 3000;
 
 app.get('/api/anroll/episodes/:animeId', async (req, res) => {
-    const animeId = req.params.animeId;
-    const url = `https://www.anroll.net/a/${animeId}`;
+  const animeId = req.params.animeId;
+  const url = `https://www.anroll.net/a/${animeId}`;
 
-    try {
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-        const episodes = [];
-        $('li:has(a[href^="/e/"])').each((index, element) => { 
-            const episodeLink = $(element).find('a').attr('href');
-            const episodeTitle = $(element).find('.titulo_episodio').text().trim();
-            const episodeNumber = $(element).find('.n_episodio span').text().trim();
-            const imageUrl = $(element).find('img').attr('src');
+    const episodes = [];
+    $('.content_ep').each((index, element) => {
+      const episodeLink = $(element).find('a').attr('href');
+      const episodeTitle = $(element)
+        .find('.titulo_episodio')
+        .text()
+        .trim();
+      const episodeNumber = $(element)
+        .find('.n_episodio span')
+        .text()
+        .trim();
+      const imageUrl = $(element).find('img').attr('src');
 
-            episodes.push({
-                episodeLink,
-                episodeTitle,
-                episodeNumber,
-                imageUrl,
-            });
-        });
+      episodes.push({
+        episodeLink,
+        episodeTitle,
+        episodeNumber,
+        imageUrl,
+      });
+    });
 
-        res.json(episodes);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
+    res.json(episodes);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
